0e4d818192341a39eb9fb71dfb40f50a34273953
bondarenkovm <bondarenko.vasyl.m@gmail.com>
2023-03-02 00:41:32 +0200

    App

---
+++ src/components/App/App.jsx
@@ -0,0 +1,32 @@
+import { Component } from 'react';
+import { Container } from './App.styled';
+
+class App extends Component {
+  // state = {
+  //   imageNane: null,
+  //   loading: false,
+  // };
+
+  // componentDidMount() {
+  //   this.setState({ loading: true });
+  //   fetch(
+  //     'https://pixabay.com/api/?key=32537245-4f388be37c0f6e70af9a9106a&q=cat&image_type=photo&orientation=horizontal&safesearch=true&per_page=12&page=1'
+  //   )
+  //     .then(res => res.json())
+  //     .then(data => this.setState({ imageNane: data }))
+  //     .finally(() => this.setState({ loading: false }));
+  // }
+  render() {
+    // const { imageNane, loading } = this.state;
+
+    return (
+      <Container>
+        {/* {loading && <div>Loading...</div>}
+        {imageNane && <div>Foto</div>} */}
+        React homework template
+      </Container>
+    );
+  }
+}
+
+export { App };
