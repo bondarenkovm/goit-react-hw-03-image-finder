341bcb9bbb2e0e42e01699fd37699e4689603452
bondarenkovm <bondarenko.vasyl.m@gmail.com>
2023-03-04 17:39:23 +0200

    Loader

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,17 +1,29 @@
-import { RotatingLines } from 'react-loader-spinner';
-import { LoaderWrapper } from './Loader.styled';
+import { TailSpin } from 'react-loader-spinner';
+// import { LoaderWrapper } from './Loader.styled';
 
 function Loader() {
   return (
-    <LoaderWrapper>
-      <RotatingLines
-        strokeColor="#4fa94d"
-        strokeWidth="5"
-        animationDuration="0.75"
-        width="80"
-        visible={true}
-      />
-    </LoaderWrapper>
+    <TailSpin
+      height="80"
+      width="80"
+      color="#4fa94d"
+      ariaLabel="tail-spin-loading"
+      radius="1"
+      wrapperStyle={{}}
+      wrapperClass="loader"
+      visible={true}
+    />
+
+    // <LoaderWrapper>
+    // <RotatingLines
+    //   strokeColor="#4fa94d"
+    //   strokeWidth="5"
+    //   animationDuration="0.75"
+    //   width="80"
+    //   visible={true}
+    //   wrapperClass="loader"
+    // />
+    // </LoaderWrapper>
   );
 }
 export { Loader };
